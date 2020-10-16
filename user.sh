d9cb7a781c10fcd7fac19a89074d2d4be6a12279
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-16 22:30:27 +0530

    Added : Validity of first name

--- user.sh
+++ user.sh
@@ -1,1 +1,12 @@
-echo "Welcome"
+#!/bin/bash -x
+echo "Welcome to User Registration"
+shopt -s extglob
+pat="^[A-Z][a-z]{2,}$"
+read -p "Enter your First name" Fname
+
+if [[ $Fname =~ $pat ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
